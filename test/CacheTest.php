c79bddd2d3e846487b0aa7e7d9229d3bcad25ea6
Andrey Ryzhov <dune@rznlf.ru>
2018-06-18 10:58:42 +0300

    Add blank code. Ready to test.

---
+++ test/CacheTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * PHP version >= 5.6
+ *
+ * @package andydune/retain-cache-on-data-absent
+ * @link  https://github.com/AndyDune/RetainCacheOnDataAbsent for the canonical source repository
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @author Andrey Ryzhov  <info@rznw.ru>
+ * @copyright 2018 Andrey Ryzhov
+ */
+
+namespace AndyDuneTest\RetainCacheOnDataAbsent;
+
+class CacheTest
+{
+
+}
